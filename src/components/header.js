316b7263ca332ce2d9fcdd6e77f65222fad5e573
dragonprevost <dragon@dkp.io>
2019-06-27 18:01:58 -0700

    Made Header

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,53 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+import Tabs from "./tabs"
+
+const tabs = [
+  {to: '/', text: 'Home'},
+  {to: '/portfolio', text: 'Portfolio'},
+  {to: '/resume', text: 'Resume'},
+  {to: '/contact', text: 'Contact'},
+];
+
+const createTabs = () => {
+  let ret = [];
+  tabs.forEach(element => {
+    ret.push(
+      <h3 style={{ margin: 0 }}>
+        <Link
+          to={element.to}
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+            padding: `0px 7px`,
+          }}
+        >
+          {element.text}  
+        </Link>
+      </h3>
+    );
+  });
+  return ret
+}
+
+const Header = ({ siteTitle, tabs }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `grey`,
       marginBottom: `1.45rem`,
+      display: `flex`,
+      justifyContent: `space-between`,
+      alignItems: `center`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h3 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -26,8 +58,13 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
         </Link>
-      </h1>
+      </h3>
     </div>
+
+    <Tabs>
+      { createTabs() }
+    </Tabs>
+
   </header>
 )
 
